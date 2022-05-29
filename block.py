a82b34b44dd4a8538c597d066f287f9b3d3f382b
David De Quesada <dquesda969x@gmail.com>
2022-05-28 23:43:58 -0400

    added block sized

---
+++ block.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+# Default value of the blocks
+BLOCK_SIZE = 2 << 13 # 16KB
+
+
+class Block():
+    
+    def __init__(self,  data: bytes = b''):
+        self.data = data
