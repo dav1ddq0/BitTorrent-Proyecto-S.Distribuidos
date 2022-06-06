2695eb98f17177c79d5c9c72adc8c4f8a84a5df6
David De Quesada <dquesda969x@gmail.com>
2022-06-06 11:18:55 -0400

    :.

--- fileinfo.py
+++ fileinfo.py
@@ -1,6 +1,13 @@
 class FileInfo:
 
-    def __init__(self, name, lenght, md5sum = None):
-        self.name = name
+    def __init__(self, path: list[str], lenght, md5sum = None):
+        self.path =  path
         self.lenght = lenght
         self.md5sum = md5sum
+
+class MultiFileInfo:
+    def __init__(self, name: str, files: dict[str, ] ):
+        self.files = files
+        self.lenght = sum(f.lenght for f in files)
+        self.md5sum = None
+        self.name = None
