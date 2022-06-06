0d96049ca3355fe3b92e23e82f27596671c50ebd
David De Quesada <dquesda969x@gmail.com>
2022-06-06 10:24:36 -0400

    :.

---
+++ fileinfo.py
@@ -0,0 +1,6 @@
+class FileInfo:
+
+    def __init__(self, name, lenght, md5sum = None):
+        self.name = name
+        self.lenght = lenght
+        self.md5sum = md5sum
--- piece.py
+++ piece.py
@@ -1,8 +1,18 @@
 # Bittorrent piece
+import math
 
+from block import BLOCK_SIZE, Block
 
 class Piece:
+
+
     def __init__(self, piece_index: int, piece_size: int, piece_hash: str):
+        """
+        Initialize a piece
+        """
         self.piece_index = piece_index
         self.piece_size = piece_size
         self.piece_hash = piece_hash
+        self.number_of_blocks: int = int(math.ceil(float(piece_size) / BLOCK_SIZE))
+        self.blocks: list['Block'] = []
+        
