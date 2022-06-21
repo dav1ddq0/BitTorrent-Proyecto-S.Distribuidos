b609136511425c3f5421864a08812383ec283575
Belzico <61699417+Belzico@users.noreply.github.com>
2022-06-21 10:26:55 -0400

    app de consola

---
+++ app-client/app_console.py
@@ -0,0 +1,27 @@
+
+
+def main():
+
+    print("Hola! Esta usando Super-Mytorrent.")
+    while True:
+        print("\n\n")
+        print("Escriba 1 para crear un torrent./n")
+        print("Escriba 2 para descargar un torrent./n")
+        print("Escriba 3 para salir de Super-Mytorrent./n")
+        print("(*-*)/n")
+        userAction= input()
+        
+        print("asdfasdfasd")
+        
+        match userAction:
+            case "1":
+                pass
+            case "2":
+                pass
+            case "3":
+                break
+            case default:
+                print("Esa opción no existe./n")
+        print("asdfasdfasd")
+main()
+print("Finish!!!")
