28e6a826914faf2f6d45ddcd2d28543d6b6716f6
David De Quesada <dquesda969x@gmail.com>
2022-06-21 11:30:30 -0400

    :.

--- piece.py
+++ piece.py
@@ -37,11 +37,7 @@ class Piece:
         blocks.append(Block(block_size = self.piece_size%BLOCK_SIZE))
         return blocks
         
-    def write_to_disk(self, path):
-        new_file = open(path, 'r+b')
-        new_file.seek(self.piece_offset)
-        new_file.write(self.raw_data)
-        new_file.close()
+    
 
 
         
