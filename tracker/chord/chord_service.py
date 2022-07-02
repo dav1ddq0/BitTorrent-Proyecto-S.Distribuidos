1fda7341c656aace5039aed9978b6e8c13dc1468
Belzico <61699417+Belzico@users.noreply.github.com>
2022-07-01 20:10:57 -0400

    implemented replication
    no esta debugueado

--- tracker/chord/chord_service.py
+++ tracker/chord/chord_service.py
@@ -1,6 +1,7 @@
 from hashlib import sha1
 from threading import Timer
 from typing import Any, Union
+from aiohttp import Fingerprint
 
 import rpyc
 
@@ -8,6 +9,9 @@ import globals_tracker
 from tracker.chord.peer_info import PeerInfo
 from tracker.chord.server_config import CHORD_NODE_PORT
 from tracker.tracker_logger import logger
+
+import copy
+import tools
 
 
 class ChordService(rpyc.Service):
@@ -31,6 +35,26 @@ class ChordService(rpyc.Service):
 
     def exposed_store_key(self, key: str, value: Any):
         return self.chord_node.store_key(key, value)
+    
+    ###########
+    #estos copian los 3 elementos para efectuar la replicacion
+    def copy_keys_regular(self):
+        return self.chord_node.dht
+    
+    def exposed_copy_keys_replic(self):
+        return self.chord_node.dht_replic
+    
+    def exposed_copy_keys_mark(self):
+        return self.chord_node.replic_list_marks
+    
+    #retorna la lista de succesores para actualizar el metodo 1 de replicacion
+    def exposed_fix_succesors(self):
+        return self.chord_node.successors_list
+    
+    #devuelve el hash del nodo
+    def exposed_hash_val(self):
+        return self.chord_node.node_val
+
 
 
 class ChordConnection:
@@ -71,6 +95,11 @@ class ChordNode:
         self.dht: dict[int, dict[str, Any]] = {}
         self.next_to_fix: int = 0
         self.finger_table: list[str] = [""] * 161
+        #news
+        self.successors_list=[]
+        self.successor_list_count=3
+        self.dht_replic: dict[int, dict[str, Any]] = {}
+        self.replic_list_marks=[]
         self.run_bg_tasks()
 
     def run_bg_tasks(self):
@@ -86,6 +115,174 @@ class ChordNode:
     #         self.predecessor,
     #     )
 
+    #ffffffffffffffffffffff
+    #en este metodo se va a revisar si el succesor existe, de no ser asi se elimina como sucesor y se llaman a los metodos de union al anillo
+    def check_successor(self):
+        if self.successor==self.node_ip:
+            return
+        with ChordConnection(self.successor) as chord_conn:
+            
+            if chord_conn==None:
+                logger.info(
+                "Node with ip %s is unreacheable.",
+                self.successor,
+            
+                )
+                self.update_when_succ_drop()
+            else:
+                self.fix_succesors()
+                
+        
+    #busca por la lista de sucesores para actualizar su sucesor, si no logra conectarse a ninguno lo busca por la finger table
+    def update_when_succ_drop(self):
+        delete_list=[]
+        self.successor
+        for item in self.successors_list:
+
+            with ChordConnection(item) as chord_conn:
+                if chord_conn!=None:
+                    self.successor=item
+                    break
+                else:
+                    delete_list.append(item)
+        
+        if len(self.successors_list)==len(delete_list):
+            #como la recorremos?
+            for i in range(0,len(self.finger_table)-1):
+                if self.ip==self.finger_table[i]:
+                    continue
+                
+                with ChordConnection(self.finger_table[i]) as chord_conn:
+                    if self.ip==self.finger_table[i]:
+                        self.finger_table[i]=self.successor
+                        break
+                    if chord_conn==None:
+                        delete_list.append(item)
+                        
+                    else :
+                        self.successor=self.finger_table[i]
+                        break
+            pass
+        
+        for item in delete_list:
+            self.successors_list.remove(item)
+            logger.info(
+            "Node with ip %s is unreacheable. Will be deleted from the successors list.",
+            item,
+        )
+        #aca aplicar la busqueda de succesors mediante la finger table
+        
+        
+        self.fix_succesors()
+
+    # en este metodo se le pide al succesor su lista de sucesores, y se actualiza la nueva lista
+    #fffffffffffffffffff
+    def fix_succesors(self):
+        if self.successor==self.node_ip:
+                self.successors_list=[]
+                return
+        with ChordConnection(self.successor) as chord_conn:
+            
+            succ_list=chord_conn.fix_succesors()
+            real_succ_list=tools.rpyc_deep_copy(succ_list)
+            real_succ_list.insert(0,self.successor)
+            while len(real_succ_list)>self.successor_list_count+1:
+                real_succ_list.pop(len(real_succ_list)-1)
+            self.successors_list=real_succ_list    
+
+    
+    #en los tres metodos el nodo se conecta a su predecessor y le pide la info para guardarla
+    def copy_keys_replica(self):
+        with ChordConnection(self.predecessor) as chord_conn:
+            if self.predecessor==self.node_ip:
+                return
+            
+            #los valores del nodo K-1
+            temp_pred_dht=chord_conn.copy_keys_regular()
+            pred_dht=tools.rpyc_deep_copy(temp_pred_dht)
+            
+            #lso valores del los otos predecesores
+            temp_pred_dht_replic=chord_conn.copy_keys_replic()
+            pred_dht_replic=tools.rpyc_deep_copy(temp_pred_dht_replic)
+            
+            #la lista de marcas de hash de predecesores
+            temp_pred_marks=chord_conn.copy_keys_mark()
+            pred_marks=tools.rpyc_deep_copy(temp_pred_marks)
+            
+            
+            temp_pred_hash=chord_conn.hash_val()
+            pred_hash=tools.rpyc_deep_copy(temp_pred_hash)
+            
+            #aca actualizamos la lista de hash de los k predecesores para la replicacion
+            if self.successor_list_count>=len(pred_marks):
+                pred_marks.pop(0)
+            pred_marks.append(pred_hash)
+            self.replic_list_marks=pred_marks
+            
+            if self.dht_replic=={}:
+                self.dht_replic=pred_dht
+            
+            else:
+                #esto puede manejarse de otra forma quizas
+                for item in pred_dht:
+                    if item in self.dht_replic:
+                        del self.dht_replic[item]
+                    
+                    self.dht_replic[item]=pred_dht[item]
+                
+                for item in pred_dht_replic:
+                    
+                    if item in self.dht_replic:
+                        del self.dht_replic[item]
+                    
+                    self.dht_replic[item]=pred_dht_replic[item]
+                self.deleted_exceded_keys
+                
+    
+    def deleted_exceded_keys(self):
+        delete_list=[]
+        #arreglar el manejo con la congruencia
+        for item in self.dht_replic:
+            
+            if self.in_range_incl(item,self.replic_list_marks[0],self.node_val):
+                delete_list.append(item)
+        for item in delete_list:
+            del self.dht_replic[item]
+
+    
+        
+    #este metodo se encarga de actualizar las replicas de los nodos direccion succ->pred
+    def update_predecessor_dic(self):
+        if self.successor==self.node_ip:
+                self.successors_list=[]
+                return
+        with ChordConnection(self.successor) as chord_conn:
+            temp_suc_dht_replic=chord_conn.copy_keys_replic()
+            suc_dht_replic=tools.rpyc_deep_copy(temp_suc_dht_replic)
+            
+            temp_suc_dht=chord_conn.copy_keys_regular()
+            suc_dht=tools.rpyc_deep_copy(temp_suc_dht)
+            
+            temp_pred_marks=chord_conn.copy_keys_mark()
+            pred_marks=tools.rpyc_deep_copy(temp_pred_marks)
+            
+            for item in suc_dht:
+                if self.in_range_incl(item,len(self.replic_list_marks[len(self.replic_list_marks)-1],self.node_val)):
+                    if item not in self.dht:
+                        self.dht[item]=suc_dht[item]
+                        
+                
+            for item in suc_dht_replic:
+                if self.in_range_incl(item,len(self.replic_list_marks[len(self.replic_list_marks)-1],self.node_val)):
+                    if item not in self.dht:
+                        self.dht[item]=suc_dht[item]
+                    elif self.in_range_incl(item,len(self.replic_list_marks[0],self.node_val)):
+                        if item not in self.dht_replic:
+                            self.dht_replic[item]=suc_dht[item]
+            
+            self.deleted_exceded_keys()
+            
+    
     def find_successor(self, node_val: int) -> str:
         if (
             ChordNode.in_range_incl(node_val, self.node_val, hash_str(self.successor))
